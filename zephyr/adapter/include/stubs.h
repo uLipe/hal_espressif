cb05cbd12f1833ddfea8316cabe0578883cacc33
Sylvio Alves <sylvio.alves@espressif.com>
2020-10-08 17:07:39 -0300

    wifi: move wifi cmake list into hal folder

---
+++ zephyr/adapter/include/stubs.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Espressif Systems (Shanghai) Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _STUBS_H_
+#define _STUBS_H_
+
+void abort(void);
+
+#endif /* _STUBS_H_ */
